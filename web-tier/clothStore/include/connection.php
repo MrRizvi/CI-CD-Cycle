23d5678daffd4c80e5e501bdec6dc3041e9b932e
root <root@ip-172-31-2-55.ap-south-1.compute.internal>
2018-12-12 05:27:08 +0000

    A

--- web-tier/clothStore/include/connection.php
+++ web-tier/clothStore/include/connection.php
@@ -1,5 +1,5 @@
 <?php
-$con =  new mysqli("13.233.167.2","prateek","redhat","dm");
+$con =  new mysqli("13.233.103.140","prateek","redhat","dm");
 if($con->connect_error)
 {
    echo $con->connect_error;
